ade948135b258f9edb3a9b481cac330f26d9b95e
zappyfish <liamkelly2015@gmail.com>
2016-11-18 20:40:43 -0500

    setup

---
+++ sinusoidtest/sinusoidtest.ino
@@ -0,0 +1,13 @@
+#define radius .0635; // 2.5 inches is .0635 meters
+#define LEDwidth .005; // 5 mm / 5 mm
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
